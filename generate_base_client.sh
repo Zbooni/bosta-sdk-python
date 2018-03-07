2748f6b97bcabd08855ac00dc145c0dcbf8c1fa4
Lemuel Formacil <lemuelf@gmail.com>
2018-03-07 08:47:18 +0400

    Update `generate_base_client.sh` script to expose `query_params` attr

--- generate_base_client.sh
+++ generate_base_client.sh
@@ -1,4 +1,4 @@
 cd stone/
-python -m stone.cli python_types ../bosta ../spec/*.stone -a url_param -a has_body
-python -m stone.cli python_client ../bosta ../spec/*.stone -a url_param -a has_body -- -m base -c BostaBase -t bosta
+python -m stone.cli python_types ../bosta ../spec/*.stone -a url_param -a query_params -a has_body
+python -m stone.cli python_client ../bosta ../spec/*.stone -a url_param -a query_params -a has_body -- -m base -c BostaBase -t bosta
 cd ..
